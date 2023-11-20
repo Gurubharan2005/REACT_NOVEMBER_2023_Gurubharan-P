2e5cf2079e1689283a01ec0461211753ea8fd5d2
Gurubharan <124855201+Gurubharan2005@users.noreply.github.com>
2023-11-20 14:53:56 +0530

    Add files via upload

---
+++ Day1 src/components/Helloworld.js
@@ -0,0 +1,15 @@
+function Hello()
+{
+    return(
+        <forms>
+            <fieldset>
+                <label><h1>ADDITION OF NUMBERS</h1></label>
+                <h2 id="n1">Enter a number1</h2><input></input>
+                <h2 id="n2">Enter a number2</h2><input></input>
+                <br></br>
+                <button onclick="add()">Submit</button>
+            </fieldset>
+        </forms>
+    )
+}
+export default Hello;
